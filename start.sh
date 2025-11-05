3db2a7f471fc84aebcdadcab310429c1475ec5b0
Leisy <leisysado@gmail.com>
2025-11-04 22:00:29 -0500

    feat: fix

--- start.sh
+++ start.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
-set -e  # Detiene la ejecución si ocurre un error
-
-echo "🚀 Iniciando despliegue del backend FastAPI en Railway..."
-
-# Ir a la carpeta del backend
-cd backend
-
-# Instalar dependencias
-echo "📦 Instalando dependencias..."
+set -e
 pip install --no-cache-dir -r requirements.txt
-
-# Lanzar el servidor FastAPI
-echo "🌐 Iniciando servidor en el puerto $PORT..."
+cd backend
 exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
